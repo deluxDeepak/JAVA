6a08048087c65c924eaf16c3aa396cf21ddafbef
deluxDeepak <154744762+deluxDeepak@users.noreply.github.com>
2024-10-01 21:17:02 +0000

    type conversion

---
+++ Basic_ofJava/03_converstion.js
@@ -0,0 +1,13 @@
+let score="34"
+console.log(typeof(score));
+
+// coverting the data type 
+let valueInNUmber=Number(score)
+console.log(typeof valueInNUmber)
+/*
+here 
+33 is converted into number 
+333sndkns => NaN  means not a number   |||type is number 
+true=>1 || false=> converted into 0
+*/
+
