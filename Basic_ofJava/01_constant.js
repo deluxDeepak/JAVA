69e6c0beabce0e4786f244195718ded3a3e3fa16
deluxDeepak <154744762+deluxDeepak@users.noreply.github.com>
2024-10-01 20:56:16 +0000

    data type & constant

---
+++ Basic_ofJava/01_constant.js
@@ -0,0 +1,24 @@
+// const keyword the value is fixed 
+const acountId=14553
+
+// it is also used for to define the variable  but it is not that much used 
+// because there is issue in block scope and functional scope 
+var account_password="37384"
+
+// let is used to define the variable 
+let account_email="deepak@gmail.com"
+
+// this is also possible to define variable like this but dont use this 
+city_name="jaipur"
+
+// this will show variable is undefine 
+let account_status
+
+// this can not be change 
+// acountId=45
+
+// this will display the output 
+console.log(acountId);
+
+// to see teh data in tabular form  and passing the multi argumrnt 
+console.table([acountId, account_email,account_password,city_name,account_status])
---
+++ Basic_ofJava/02_data_type.js
@@ -0,0 +1,16 @@
+"USE STRICT";              // treat all js code as newer 
+// alert(4+5)               //here we can not use this ,use in browser 
+
+
+// 1.Number
+// bigint 
+// String
+// Boolean
+// null=> standalone value means empty  |type of null is undefined 
+// undefined ||type of indefined is undefind 
+// Symbol=>unique to define uniquness 
+
+
+// Object
+console.log(typeof "dhbfj")
+
